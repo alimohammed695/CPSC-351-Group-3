3fb998b12a43047561573f40d8c0e669714138bf
AnOrdinaryUsername <kylemasa2@gmail.com>
2022-02-24 14:16:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Group 4 (CPSC 351)
+
+## Getting Started
+
+IDE preference is up to you. I recommend VSCode since there are some settings I already configured
+in the `.vscode` folder.
+
+I'm also assuming you're using Linux/MacOS for compiling.
+
+### Visual Studio Code
+
+ - Install [Clangd](https://marketplace.visualstudio.com/items?itemName=llvm-vs-code-extensions.vscode-clangd)
---
+++ assignment-1/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void print_hex(string a) {
+    const double pi = 3.14569 * 2;
+    cout << "TEST" << endl;
+    cout << a << endl;
+
+
+class Testa {
+   public:
+    int a;
+
+   private:
+    int b;
+};
+
+struct AnotherOne {
+    int te;
+};
+
+int main() {
+    bool val = true;
+
+    if (val) {
+        cout << "test" << endl;
+    }
+
+    cout << "test" << endl;
+    print_hex("s");
+    return 0;
+}
